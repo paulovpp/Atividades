4a28d6361595df0c59e45f0f2fa21285c6584904
Paulo <paulovpp@gmail.com>
2021-11-22 15:20:42 -0300

    first commit with a few questions

---
+++ class2-qst1.py
@@ -0,0 +1,25 @@
+# Practical activity 2
+# Class 2
+# Paulo Pinheiro
+
+from sympy import *
+
+# Question 1
+
+""" def profit(marg):
+    return (marg + 11500)/(300 * 0.725)
+
+num = float(input('Digite: '))
+print(profit(num)) """
+
+# Question 2
+
+x, y = symbols("x y")
+init_printing()
+print(expand((5*x + 3*y + 2*x*y)**3))
+
+# Question 3
+
+
+
+# Question 4
